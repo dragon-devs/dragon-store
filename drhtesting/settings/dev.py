88244f1b41a0aa3120d58bfac26f44eeae862a77
Ghost <Ghost@dragondevs.com>
2022-12-27 23:52:43 +0500

    new styles and db settings change

--- drhtesting/settings/dev.py
+++ drhtesting/settings/dev.py
@@ -4,11 +4,8 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dragonstore',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'Brucelee47'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
